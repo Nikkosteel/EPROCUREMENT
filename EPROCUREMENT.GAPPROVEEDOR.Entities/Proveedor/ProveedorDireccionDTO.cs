e5bd2f13dc412568d5fe6941de07cc7bfd07bce0
msalgado008 <37191572+msalgado008@users.noreply.github.com>
2019-11-09 02:19:17 -0600

    Ajuste de proveedor Direccion
    Proveedor Dirección

--- EPROCUREMENT.GAPPROVEEDOR.Entities/Proveedor/ProveedorDireccionDTO.cs
+++ EPROCUREMENT.GAPPROVEEDOR.Entities/Proveedor/ProveedorDireccionDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EPROCUREMENT.GAPPROVEEDOR.Entities.Proveedor
+namespace EPROCUREMENT.GAPPROVEEDOR.Entities
 {
     public class ProveedorDireccionDTO
     {
