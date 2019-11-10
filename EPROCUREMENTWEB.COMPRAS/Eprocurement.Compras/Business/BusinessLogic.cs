ef4e7d7ee8636374521275e1d84bbd4759a27cd3
Victor de la Torre <36996708+Nikkosteel@users.noreply.github.com>
2019-11-10 02:22:49 -0600

    Proyecto de Compras

---
+++ EPROCUREMENTWEB.COMPRAS/Eprocurement.Compras/Business/BusinessLogic.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Script.Serialization;
+using EPROCUREMENT.GAPPROVEEDOR.Entities;
+
+namespace Eprocurement.Compras.Business
+{
+    public class BusinessLogic
+    {
+        string urlApi = ConfigurationManager.AppSettings["urlApi"].ToString();
+        public List<PaisDTO> GetPaisesList()
+        {
+            var lstPaises = new List<PaisDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("PaisGetList");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<PaisResponseDTO>(readTask.Result);
+                    lstPaises = response.PaisList;
+                }
+            }
+            return lstPaises;
+        }
+        public List<AeropuertoDTO> GetAeropuertosList()
+        {
+            var lstAeropuertos = new List<AeropuertoDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("AeropuertoGetList");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<AeropuertoResponseDTO>(readTask.Result);
+                    lstAeropuertos = response.AeropuertoList;
+                }
+            }
+            return lstAeropuertos;
+        }
+        public List<GiroDTO> GetGirosList()
+        {
+            var lstGiros = new List<GiroDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("GiroGetList");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<GiroResponseDTO>(readTask.Result);
+                    lstGiros = response.GiroList;
+                }
+            }
+            return lstGiros;
+        }
+        public List<NacionalidadDTO> GetNacionalidadList()
+        {
+            var lstNacionalidad = new List<NacionalidadDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("NacionalidadGetList");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<NacionalidadResponseDTO>(readTask.Result);
+                    lstNacionalidad = response.NacionalidadList;
+                }
+            }
+            return lstNacionalidad;
+        }
+
+        public List<ZonaHorariaDTO> GetZonaHorariaList()
+        {
+            var lstZonaHoraria = new List<ZonaHorariaDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("ZonaHorariaGetList");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<ZonaHorariaResponseDTO>(readTask.Result);
+                    lstZonaHoraria = response.ZonaHorariaList;
+                }
+            }
+            return lstZonaHoraria;
+        }
+
+        public List<IdiomaDTO> GetIdiomaList()
+        {
+            var lstIdioma = new List<IdiomaDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("IdiomaGetList");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<IdiomaResponseDTO>(readTask.Result);
+                    lstIdioma = response.IdiomaList;
+                }
+            }
+            return lstIdioma;
+        }
+
+        //public List<EstadoDTO> GetEstadoList()
+        //{
+        //    var lstEstado = new List<EstadoDTO>();
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+        //        var responseTask = client.GetAsync("EstadoGetList");
+        //        responseTask.Wait();
+
+        //        var result = responseTask.Result;
+        //        if (result.IsSuccessStatusCode)
+        //        {
+        //            var readTask = result.Content.ReadAsStringAsync();
+        //            JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+        //            var response = JSserializer.Deserialize<EstadoResponseDTO>(readTask.Result);
+        //            lstEstado = response.EstadoList;
+        //        }
+        //    }
+        //    return lstEstado;
+        //}
+
+        //public List<MunicipioDTO> GetMunicipioList()
+        //{
+        //    var lstMunicipio = new List<MunicipioDTO>();
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+        //        var responseTask = client.GetAsync("MunicipioGetList");
+        //        responseTask.Wait();
+
+        //        var result = responseTask.Result;
+        //        if (result.IsSuccessStatusCode)
+        //        {
+        //            var readTask = result.Content.ReadAsStringAsync();
+        //            JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+        //            var response = JSserializer.Deserialize<MunicipioResponseDTO>(readTask.Result);
+        //            lstMunicipio = response.MunicipioList;
+        //        }
+        //    }
+        //    return lstMunicipio;
+        //}
+
+        public List<TipoProveedorDTO> GetTipoProveedorList()
+        {
+            var lstTipoProveedor = new List<TipoProveedorDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlApi + "api/Catalogo/");
+                var responseTask = client.GetAsync("TipoProveedorGetList");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    JavaScriptSerializer JSserializer = new JavaScriptSerializer();
+
+                    var response = JSserializer.Deserialize<TipoProveedorResponseDTO>(readTask.Result);
+                    lstTipoProveedor = response.TipoProveedorList;
+                }
+            }
+            return lstTipoProveedor;
+        }
+    }
+}
---
+++ EPROCUREMENTWEB.COMPRAS/Eprocurement.Compras/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Eprocurement.Compras.Business;
+using EPROCUREMENT.GAPPROVEEDOR.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eprocurement.Compras.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        public List<AeropuertoDTO> aeropuertoList;
+        public List<GiroDTO> giroList;
+        public List<TipoProveedorDTO> tipoProveedorList;
+
+        private void CargarCatalogos()
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+            aeropuertoList = businessLogic.GetAeropuertosList();
+            giroList = businessLogic.GetGirosList();
+            tipoProveedorList = businessLogic.GetTipoProveedorList();
+          
+          
+        }
+
+        public ActionResult Index()
+        {
+            CargarCatalogos();
+            
+            ViewBag.AeropuertoList = aeropuertoList;
+            ViewBag.GiroList = giroList;
+            ViewBag.TipoProveedorList = tipoProveedorList;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
